7592e782acb6230efd99b025223e9bd3311c26e3
Pera Urosevic <pera.urosevic@gmail.com>
2023-01-27 15:19:26 +0200

    heartbeat

---
+++ src/monitor/heartbeat.rs
@@ -0,0 +1,17 @@
+extern crate redis;
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use crate::database::DB;
+
+pub fn task() {
+    let time = SystemTime::now();
+    let ts = time
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let timestamp = format!("{}", ts.as_millis());
+    // println!("[heartbeat] {}", timestamp);
+    DB::new()
+        .del("pier:heartbeat")
+        .hset("pier:heartbeat", "timestamp", timestamp);
+}
--- src/monitor/mod.rs
+++ src/monitor/mod.rs
@@ -2,6 +2,7 @@ use clokwerk::{Scheduler, TimeUnits};
 
 mod alert;
 mod cpu;
+mod heartbeat;
 mod mem;
 mod storage;
 mod swap;
@@ -15,10 +16,12 @@ pub fn run() -> Scheduler {
     cpu::task();
     mem::task();
     swap::task();
+    heartbeat::task();
     scheduler.every(1.hour()).run(|| uptime::task());
     scheduler.every(10.minute()).run(|| storage::task());
     scheduler.every(10.second()).run(|| cpu::task());
     scheduler.every(10.second()).run(|| mem::task());
     scheduler.every(10.second()).run(|| swap::task());
+    scheduler.every(1.second()).run(|| heartbeat::task());
     return scheduler;
 }
